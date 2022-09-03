fa4b28d1d35172d44236f44f475c22dcbe76cbf0
Nalu Zou <zonalu@amazon.com>
2022-09-03 15:08:43 -0700

    Scrape words world

--- code_names_bot_oxford_downloader/create_wordlist.py
+++ code_names_bot_oxford_downloader/create_wordlist.py
@@ -1,15 +1,21 @@
 import os
 
-from config import WORDLIST_PATH, PREMIUM_OXFORD_WORD_LIST_DIR_US
+from config import WORDLIST_PATH, PREMIUM_OXFORD_WORD_LIST_DIR_US, PREMIUM_OXFORD_WORD_LIST_DIR_WORLD
 
 WORD_FREQ_THRESHOLD = 1e-6
 
 
 def main():
-    lemmas = []
+    lemmas = set()
     for file_name in os.listdir(PREMIUM_OXFORD_WORD_LIST_DIR_US):
         with open(os.path.join(PREMIUM_OXFORD_WORD_LIST_DIR_US, file_name), "r") as file:
-            lemmas += file.read().splitlines()
+            lemmas += set(file.read().splitlines())
+
+    for file_name in os.listdir(PREMIUM_OXFORD_WORD_LIST_DIR_WORLD):
+        with open(os.path.join(PREMIUM_OXFORD_WORD_LIST_DIR_US, file_name), "r") as file:
+            lemmas += set(file.read().splitlines())
+    
+    lemmas = list(lemmas)
 
     with open(WORDLIST_PATH, "w+") as file:
         file.write("\n".join(lemmas))
--- code_names_bot_oxford_downloader/scraper/premium_word_list_scraper.py
+++ code_names_bot_oxford_downloader/scraper/premium_word_list_scraper.py
@@ -6,11 +6,6 @@ from .scraper import scrape
 
 WORD_OVERRIDES = {"°": "°"}
 
-GET_URL = (
-    lambda item: f"https://premium-oxforddictionaries-com.offcampus.lib.washington.edu/us/browse/american_english/{item.split('|')[0]}/{item.split('|')[1]}/"
-)
-
-
 def extract(soup):
     output = []
 
@@ -28,6 +23,7 @@ def format_word(word):
 
     word = word.replace("\u2013", "<")  # Preserve en dash
     word = word.replace("\u2014", ">")  # Preserve em dash
+    word = word.replace("/", "{")  # Replace forward slash so file name is valid. Will reverse when getting URL. (sacrifice-someone/-on-the-altar-of)
     word = (
         unidecode(word).lower().replace(" ", "-").replace(",", "").replace("?", "%3F")
     )
@@ -54,7 +50,7 @@ def scrape_premium(word_list_targets_dir, word_lists_dir, dictionary_name):
     print(format_word("Wouldn't you like to know?"))
     targets = get_targets(word_list_targets_dir)
 
-    get_url = lambda item: f"https://premium-oxforddictionaries-com.offcampus.lib.washington.edu/us/browse/{dictionary_name}/{item.split('|')[0]}/{item.split('|')[1]}/"
+    get_url = lambda item: f"https://premium-oxforddictionaries-com.offcampus.lib.washington.edu/us/browse/{dictionary_name}/{item.split('|')[0]}/{item.split('|')[1].replace('{', '/')}/"
 
     scrape(
         get_url,
@@ -63,6 +59,6 @@ def scrape_premium(word_list_targets_dir, word_lists_dir, dictionary_name):
         word_lists_dir,
         headers={
             "User-Agent": "Mozilla/6.0",
-            "Cookie": "_ga=GA1.2.1761768241.1655574857; _ga_3T65WK0BM8=GS1.1.1661471019.7.0.1661471019.0.0.0; _ga_JLHM9WH4JV=GS1.1.1661471019.7.0.1661471019.0.0.0; nmstat=10857d25-4bfe-6f03-1dc1-5298ad5578ca; _mkto_trk=id:131-AQO-225&token:_mch-washington.edu-1656568436075-85816; _ga_57P4HTBKTG=GS1.1.1658767967.2.0.1658767967.0; ezproxyuwlib=HsfMJFVlkZKnhW5; localisation=US; JSESSIONID=676F28D29FD329586B5E9C88188B6AE4; SaneID=oi6OAmvT8jP-dQNDAmp; cookieLaw=true; XSRF-TOKEN=2731da63-288e-4c96-9b6c-218f3e45aed1",
+            "Cookie": "_ga=GA1.2.1761768241.1655574857; _ga_3T65WK0BM8=GS1.1.1661471019.7.0.1661471019.0.0.0; _ga_JLHM9WH4JV=GS1.1.1661471019.7.0.1661471019.0.0.0; nmstat=10857d25-4bfe-6f03-1dc1-5298ad5578ca; _mkto_trk=id:131-AQO-225&token:_mch-washington.edu-1656568436075-85816; _ga_57P4HTBKTG=GS1.1.1658767967.2.0.1658767967.0; ezproxyuwlib=VssrCGwWlIU7USt; localisation=US; JSESSIONID=073322D30426BD41781DCDF46732DBD4; SaneID=oi6OAmvT8jP-dQNDAmp; cookieLaw=true; XSRF-TOKEN=76e016fc-872c-4865-8702-19fe67d50859",
         },
     )
