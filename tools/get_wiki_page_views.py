2fd7153b82fdedb984353f4e5eb65969662e67f9
Nalu Zou <naluzou@Nalus-MacBook-Pro.local>
2022-09-25 11:50:01 -0700

    Add wiki page views tool

---
+++ tools/get_wiki_page_views.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+from config import WIKI_PAGE_VIEWS
+
+
+def main():
+    with open(WIKI_PAGE_VIEWS, "r") as file:
+        lines = file.read().splitlines()
+        lines = [line.split(" ") for line in lines]
+        page_views = {line[0]: int(line[1]) for line in lines}
+
+    while True:
+        title = input("Enter article title:")
+        print(page_views[title])
+
+
+if __name__ == "__main__":
+    main()
