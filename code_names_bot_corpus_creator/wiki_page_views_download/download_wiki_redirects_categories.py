c3be36bafc04085015ea7dc2bbbd51be30750c3e
Nalu Zou <naluzou@Nalus-MacBook-Pro.local>
2022-09-27 15:02:17 -0700

    Lower download rate

--- code_names_bot_corpus_creator/wiki_page_views_download/download_wiki_redirects_categories.py
+++ code_names_bot_corpus_creator/wiki_page_views_download/download_wiki_redirects_categories.py
@@ -39,7 +39,7 @@ def main():
         cache=WikiRedirectsCategoriesCache(),
         process_result=process_result,
         chunk_size=20,
-        download_rate=200,
+        download_rate=100,
     )
 
 
