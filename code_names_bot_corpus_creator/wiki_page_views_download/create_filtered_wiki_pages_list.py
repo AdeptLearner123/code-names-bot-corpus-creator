c05c5060fbf2328268989a3293662223c9228286
Nalu Zou <naluzou@Nalus-MacBook-Pro.local>
2022-09-27 12:38:49 -0700

    Add filtering out lists for wiki pages

--- code_names_bot_corpus_creator/wiki_page_views_download/create_filtered_wiki_pages_list.py
+++ code_names_bot_corpus_creator/wiki_page_views_download/create_filtered_wiki_pages_list.py
@@ -12,19 +12,25 @@ def main():
 
     with open(WIKI_PAGES) as file:
         page_id_titles = file.read().splitlines()
-        page_id_titles = list(
-            map(lambda page_id_title: page_id_title.split("\t"), page_id_titles)
+        page_id_titles = map(
+            lambda page_id_title: page_id_title.split("\t"), page_id_titles
+        )
+        page_id_titles = map(
+            lambda page_id_title: (page_id_title[0], page_id_title[1]), page_id_titles
         )
 
-    filtered_page_id_titles = list(
-        filter(
-            lambda page_id_title: page_id_title[1] in page_views
-            and page_views[page_id_title[1]] > PAGE_VIEW_THRESHOLD,
-            page_id_titles,
-        )
+    filtered_by_dump_page_views = filter(
+        lambda _, title: title in page_views
+        and page_views[title] > PAGE_VIEW_THRESHOLD,
+        page_id_titles,
     )
+
+    filtered_non_lists = filter(
+        lambda _, title: not title.startswith("List_of_"), filtered_by_dump_page_views
+    )
+
     filtered_page_id_titles = list(
-        map(lambda page_id_title: "\t".join(page_id_title), filtered_page_id_titles)
+        map(lambda page_id_title: "\t".join(page_id_title), filtered_non_lists)
     )
 
     with open(DUMP_PAGE_VIEW_FILTERED, "w+") as file:
