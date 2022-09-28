26528c6f3724d240c38ddcdf0701ede3fc942d5a
Nalu Zou <naluzou@Nalus-MacBook-Pro.local>
2022-09-27 23:07:17 -0700

    Fix

--- code_names_bot_corpus_creator/wiki_filter_2/wiki_filter_2.py
+++ code_names_bot_corpus_creator/wiki_filter_2/wiki_filter_2.py
@@ -1,3 +1,4 @@
+import json
 from code_names_bot_corpus_creator.download.caches import WikiRedirectsCategoriesCache
 from config import WIKI_FILTERED_1, WIKI_FILTERED_2
 
@@ -25,23 +26,19 @@ def get_redirect_titles(json):
 def main():
     with open(WIKI_FILTERED_1) as file:
         page_id_titles = file.read().splitlines()
-        print("Page id titles", len(page_id_titles))
         page_id_titles = list(map(
             lambda page_id_title: page_id_title.split("\t"), page_id_titles
         ))
-        print("Page id titles", len(list(page_id_titles)))
         title_to_page_id = { page_id_title[1]: page_id_title[0] for page_id_title in page_id_titles}
-        print("Page id titles", len(list(page_id_titles)))
         titles = [ page_id_title[1] for page_id_title in page_id_titles ]
     
     cache = WikiRedirectsCategoriesCache()
     title_to_json = cache.get_key_to_value()
+    for title in title_to_json:
+        title_to_json[title] = json.loads(title_to_json[title])
 
-    print("Titles", len(titles))
     titles = filter(lambda title: not is_disambiguation(title_to_json[title]), titles)
-    print("Titles", len(list(titles)))
     titles = filter(lambda title: has_single_word_title(title_to_json[title]), titles)
-    print("Titles", len(list(titles)))
     title_to_redirects = { title: get_redirect_titles(title) for title in titles}
 
     with open(WIKI_FILTERED_2, "w+") as file:
