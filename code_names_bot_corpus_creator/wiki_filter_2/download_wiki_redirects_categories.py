1becdfd03ed21f8f1abf8781c7e03d7e4750a248
Nalu Zou <naluzou@Nalus-MacBook-Pro.local>
2022-09-29 14:52:22 -0700

    Reduce download rate

--- code_names_bot_corpus_creator/wiki_filter_2/download_wiki_redirects_categories.py
+++ code_names_bot_corpus_creator/wiki_filter_2/download_wiki_redirects_categories.py
@@ -90,7 +90,7 @@ def main():
         cache=WikiRedirectsCategoriesCache(),
         process_result=process_result,
         chunk_size=20,
-        download_rate=50,
+        download_rate=20,
     )
 
 
