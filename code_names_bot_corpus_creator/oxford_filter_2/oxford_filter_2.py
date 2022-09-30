f3da4b2b25fd300ff728382997765f74e6ab0a73
Nalu Zou <naluzou@Nalus-MacBook-Pro.local>
2022-09-29 22:36:13 -0700

    Include regions in oxford filter 2

--- code_names_bot_corpus_creator/oxford_filter_2/oxford_filter_2.py
+++ code_names_bot_corpus_creator/oxford_filter_2/oxford_filter_2.py
@@ -3,15 +3,14 @@ from config import OXFORD_FILTERED_1, OXFORD_FILTERED_2, MISSING_SENTENCES
 def main():
     with open(OXFORD_FILTERED_1, "r") as file:
         lemma_regions = file.read().splitlines()
-        lemmas = [lemma_region.split("|")[0] for lemma_region in lemma_regions ]
 
     with open(MISSING_SENTENCES) as file:
         missing_sentences = set(file.read().splitlines())
 
-    lemmas = filter(lambda lemma: lemma not in missing_sentences)
+    lemma_regions = filter(lambda lemma_region: lemma_region.split("|")[0] not in missing_sentences, lemma_regions)
     
     with open(OXFORD_FILTERED_2, "w+") as file:
-        file.write("\n".join(lemmas))
+        file.write("\n".join(lemma_regions))
 
 
 if __name__ == "__main__":
