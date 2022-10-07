2a1f124fb8c0319ea6e8f5eea35b0016692b7355
Nalu Zou <naluzou@Nalus-MacBook-Pro.local>
2022-10-07 12:46:44 -0700

    Add progress monitoring for wiki filter 1

--- code_names_bot_dictionary_compiler/wiki_filter_1/wiki_filter_1.py
+++ code_names_bot_dictionary_compiler/wiki_filter_1/wiki_filter_1.py
@@ -1,3 +1,5 @@
+from tqdm import tqdm
+
 from code_names_bot_dictionary_compiler.wiki_utils.entity_classifier import is_entity
 from config import DUMP_WIKI_PAGE_VIEWS, WIKI_PAGES, WIKI_FILTERED_1
 
@@ -6,28 +8,33 @@ PAGE_VIEW_THRESHOLD = 8000
 
 
 def main():
+    print("Status:", "Reading dump page views")
     with open(DUMP_WIKI_PAGE_VIEWS, "r") as file:
         lines = file.read().splitlines()
         lines = [line.split(" ") for line in lines]
         page_views = {line[0]: int(line[1]) for line in lines}
 
+    print("Status:", "Reading wiki page titles")
     with open(WIKI_PAGES) as file:
         page_id_titles = file.read().splitlines()
         page_id_titles = map(
             lambda page_id_title: page_id_title.split("\t"), page_id_titles
         )
 
+    print("Status:", "Filtering by dump page views")
     filtered_by_dump_page_views = filter(
         lambda page_id_title: page_id_title[1] in page_views
         and page_views[page_id_title[1]] > PAGE_VIEW_THRESHOLD,
         page_id_titles,
     )
 
+    print("Status:", "Reading dump page views")
     filtered_non_lists = filter(
         lambda page_id_title: is_entity(page_id_title[1]),
-        filtered_by_dump_page_views,
+        tqdm(filtered_by_dump_page_views),
     )
 
+    print("Status:", "Dumping")
     filtered_page_id_titles = list(
         map(lambda page_id_title: "\t".join(page_id_title), filtered_non_lists)
     )
